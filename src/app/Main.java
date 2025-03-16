7620a8fec4679d993ab85e25c41f3367e2d32590
Nazar <nazar31032009@gmail.com>
2025-03-16 13:34:32 +0200

    Refactor Main class

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,23 +3,29 @@ package app;
 public class Main {
 
     public static void main(String[] args) {
+
         // Отримуємо вхідні дані
         String[] data = getData();
+
         // Формуємо об'єкт з вхідних даних
         Product product = new Product(data[0],
                 Integer.parseInt(data[1]),
                 Double.parseDouble(data[2]));
+
         // Розраховуємо базову вартість товару
         CalcCostBase costBase = new CalcCostBase();
         double baseCost = costBase.calcCost(product);
+
         // Розраховуємо вартість товару, з урахуванням доставки
         CalcCostDelivery costDelivery = new CalcCostDelivery();
         double deliveryCost = costDelivery.calcCost(product);
+
         // Формуємо виведення
         String baseOutput = product + "\nCost is " +
                 Constants.CURRENCY + " " + baseCost + ".";
         String deliveryOutput = product + "\nCost is " +
                 Constants.CURRENCY + " " + deliveryCost + ".";
+
         // Виводимо результат
         getOutput(baseOutput);
         getOutput(deliveryOutput);
